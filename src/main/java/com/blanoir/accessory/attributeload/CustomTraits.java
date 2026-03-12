f38437d3607b0d6092f5fb1ddae57d9fe111ae6d
LoscX-X <loscx@qq.com>
2026-03-12 08:33:30 +0800

    (Lang) Change the display name

--- src/main/java/com/blanoir/accessory/attributeload/CustomTraits.java
+++ src/main/java/com/blanoir/accessory/attributeload/CustomTraits.java
@@ -6,31 +6,31 @@ import dev.aurelium.auraskills.api.trait.CustomTrait;
 public class CustomTraits {
     public static final CustomTrait HEAL_REGENERATION = CustomTrait
             .builder(NamespacedId.of("accessory", "heal_regeneration"))
-            .displayName("生命恢复")
+            .displayName("Heal_Regeneration")
             .build();
     public static final CustomTrait DEFENCE = CustomTrait
             .builder(NamespacedId.of("accessory", "defence"))
-            .displayName("抵御")
+            .displayName("Defence")
             .build();
     public static final CustomTrait HEAL_DECREASE = CustomTrait
             .builder(NamespacedId.of("accessory", "heal_regdecrease"))
-            .displayName("生命恢复衰减")
+            .displayName("Heal_Decrease")
             .build();
 
     public static final CustomTrait LIFE_STEAL = CustomTrait
             .builder(NamespacedId.of("accessory", "life_steal"))
-            .displayName("life_steal")
+            .displayName("Life_Steal")
             .build();
     public static final CustomTrait ABSORB = CustomTrait
             .builder(NamespacedId.of("accessory", "absorb"))
-            .displayName("护盾")
+            .displayName("Absorb")
             .build();
     public static final CustomTrait MAGICABSORB = CustomTrait
             .builder(NamespacedId.of("accessory", "magicabsorb"))
-            .displayName("魔法护盾")
+            .displayName("Magic_Absorb")
             .build();
     public static final CustomTrait HEALTH = CustomTrait
             .builder(NamespacedId.of("accessory", "health"))
-            .displayName("生命值")
+            .displayName("Health")
             .build();
 }
